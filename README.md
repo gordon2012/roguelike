1c716b0e1edecf6a6d91917a5edb461f64469c48
Gordon Doskas <gdoskas2012@email.phoenix.edu>
2016-01-03 00:33:33 -0800

    implement player

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
-#roguelike
+# roguelike
 
-##A zipline for freeCodeCamp
+## A zipline for freeCodeCamp
 
-Basis: react tutorial: http://jamesknelson.com/learn-raw-react-no-jsx-flux-es6-webpack/
+
+### Basis:
+- react tutorial: http://jamesknelson.com/learn-raw-react-no-jsx-flux-es6-webpack/
+- 2nd react tutorial: http://jamesknelson.com/learn-raw-react-ridiculously-simple-forms/
--- roguelike.js
+++ roguelike.js
@@ -1,11 +1,14 @@
+// ********
+// DATA
+// ********
 // 0=air, 1=wall, TODO: 2=player
 //
 var level =
 [
 	[1,1,1,1,1,1,1,1,1,1,1,1],
 	[1,0,0,0,0,0,0,1,0,0,0,1],
+	[1,0,2,0,0,0,0,0,0,0,0,1],
 	[1,0,0,0,0,0,0,1,0,0,0,1],
-	[1,0,0,0,0,0,0,0,0,0,0,1],
 	[1,0,0,0,0,0,0,1,1,1,1,1],
 	[1,0,0,0,0,0,0,1,0,0,0,1],
 	[1,0,0,0,0,0,0,0,0,0,0,1],
@@ -13,10 +16,13 @@ var level =
 	[1,1,1,1,1,1,1,1,1,1,1,1]
 ];
 
-var player = {x: 3, y: 5};
+// var player = {x: 3, y: 5};
+// ********
 
 
-
+// ********
+// COMPONENT: Tile
+// ********
 var Tile = React.createClass
 ({
 	propTypes:
@@ -32,9 +38,12 @@ var Tile = React.createClass
 		{
 			case 0:
 				className = className + ' Tile-air';
-			break;
+				break;
 			case 1:
 				className = className + ' Tile-wall';
+				break;
+			case 2:
+				className = className + ' Tile-player';
 		}
 
 		return (
@@ -44,8 +53,12 @@ var Tile = React.createClass
 		);
 	}
 });
+// ********
 
 
+// ********
+// COMPONENT: Level
+// ********
 var Level = React.createClass
 ({
 	render: function()
@@ -62,21 +75,81 @@ var Level = React.createClass
 		);
 	}
 });
+// ********
 
 
-// var Application = react.createClass
-// ({
-//
-// });
+// ********
+// COMPONENT: Application
+// ********
+var Application = React.createClass
+({
+	propTypes:
+	{
 
-var testLevel =
-[
-	[1,1,1,1],
-	[1,0,0,1],
-	[1,1,1,1],
-];
-// ReactDOM.render(React.createElement(Tile, {tile: 1}), document.getElementById('react-app'));
-ReactDOM.render(React.createElement(Level, {level: level}), document.getElementById('react-app'));
+	},
+
+	render: function()
+	{
+		return (
+			React.createElement('div', {className: 'Application', onKeyDown: this.handleKeyDown },
+				React.createElement('h1', {className: 'Application-title'}, 'roguelike'),
+				React.createElement(Level, {level: level})
+			)
+		);
+	},
+
+
+	componentDidMount: function()
+	{
+		// console.log(this);
+	},
+
+	handleKeyDown: function(e)
+	{
+		//console.log(_.cloneDeep(e));
+		//alert(e);
+		console.log('test');
+	}
+
+});
+// ********
+
+
+document.body.onkeydown = function(e)
+{
+	console.log(e.keyCode);
+
+
+	//var loc;
+
+	(function()
+	{
+		for(var i = 0; i < level.length; i++)
+		{
+			for(var j = 0; j < level[i].length; j++)
+			{
+				if(level[i][j] === 2)
+				{
+					//alert(i + ', ' + j);
+				}
+			}
+		}
+	})()
+}
+
+
+// ********
+// EVENT
+// ********
+// var onKeyDown = function(event)
+// {
+// 	console.log(_.cloneDeep(e));
+// 	alert(e);
+// }
+// ********
+
+
+ReactDOM.render(React.createElement(Application, {level: level}), document.getElementById('react-app'));
 
 
 
